4e0121a8e83523dc21bb240409482287da419902
Adam Eckley <adam.eckley@roq.co.uk>
2023-02-21 14:48:39 +0000

    Added tests to confirm the cat object is working as needed.

--- src/test/java/com/credersi/animals/CatTest.java
+++ src/test/java/com/credersi/animals/CatTest.java
@@ -12,4 +12,29 @@ public class CatTest {
 		assertTrue(cat.isAlive());
 		assertEquals(cat.howHungry(), 50);
 	}
+	
+	@Test
+	public void testCatRuns() {
+		Cat cat = new Cat();
+		cat.runs();
+		assertEquals(cat.howHungry(), 80);
+	}
+	
+	@Test
+	public void testCatDead() {
+		Cat cat = new Cat();
+		cat.runs();
+		cat.runs();
+		assertTrue(cat.isAlive() == false);
+	}
+	
+	@Test 
+	public void testCatFullHealth() {
+		Cat cat = new Cat();
+		cat.feeds();
+		cat.feeds();
+		cat.feeds();
+		assertEquals(cat.howHungry(), 0);
+		}
+	
 }
